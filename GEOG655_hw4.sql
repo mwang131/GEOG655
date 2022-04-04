cbe00bd9ffbbe4b56c86be8bb3ec4b961d46d0d4
miwa5437 <96434785+miwa5437@users.noreply.github.com>
2022-04-04 02:03:40 -0400

    Create GEOG655_hw4.sql

---
+++ GEOG655_hw4.sql
@@ -0,0 +1,16 @@
+CREATE TABLE categories 
+(category_code varchar PRIMARY KEY, 
+category varchar);
+ 
+CREATE TABLE businesses 
+(id integer PRIMARY KEY, 
+business varchar, 
+year_founded integer, 
+category_code varchar, 
+country_code char(3));
+ 
+CREATE TABLE countries 
+(country_code varchar PRIMARY KEY, 
+country varchar, 
+continent varchar);
+/*GEOG661, Mingyuan Wang, April 3rd, 2022.*/
